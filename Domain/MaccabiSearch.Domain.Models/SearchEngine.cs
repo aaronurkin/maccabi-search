b3b5afd9307026a24793ec20db95aa7a668babeb
Aaron Urkin <aaron@jobtestprep.com>
2024-03-10 15:10:54 +0200

    Add the `Domain` layer

---
+++ Domain/MaccabiSearch.Domain.Models/SearchEngine.cs
@@ -0,0 +1,18 @@
+﻿namespace MaccabiSearch.Domain.Models
+{
+    /// <summary>
+    /// Possible Search Engines.
+    /// </summary>
+    public enum SearchEngine
+    {
+        /// <summary>
+        /// Default value to prevent a wrong value.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Google Search Engine
+        /// </summary>
+        Google = 1,
+    }
+}
